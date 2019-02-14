51dcae51eec2d0ceec9c4944b208bfa2db1f336c
Sam Gottlieb <sam@Fernandos-iMac.local>
2019-02-14 10:14:26 +0100

    commit current files

---
+++ src/test/java/treebankMetadataValidator/validatorTest.java
@@ -0,0 +1,14 @@
+package treebankMetadataValidator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ValidatorTest {
+	/**
+	@Test
+	void testIsDirectory() {
+		Validator x = new Validator()
+	}
+	*/
+}
