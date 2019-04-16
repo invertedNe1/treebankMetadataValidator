cb696a48d2749d5af31f2a3307cf1d7667f15e39
invertedNe1 <samjgottlieb@gmail.com>
2019-04-16 10:00:45 +0200

    pushing current state

--- src/main/java/MetadataValidation.java
+++ src/main/java/MetadataValidation.java
@@ -30,12 +30,10 @@ import org.xml.sax.SAXException;
 
 public class MetadataValidation {
 	
-	// class variables
-	public ArrayList<File> fileEntries = new ArrayList<File>(); //potential place to store files in a folder.
-	//square brackets []
+	public ArrayList<File> fileEntries = new ArrayList<File>(); // potential place to store files in a folder.
 	
 	/**
-	 * constructor. Performs metadata validation for a single file, or for all files in a directory.
+	 * Performs metadata validation for a single file, or for all files in a directory.
 	 */
 	public MetadataValidation(File file) throws FileNotFoundException, IOException {
 		if (file.isDirectory()) {
@@ -62,14 +60,13 @@ public class MetadataValidation {
 	}
 	
 	/**
-	 * helper method. Checks if XML in metadata file is well formed. 
-	 * @throws IOException 
+	 * Checks if XML in metadata file is well formed. 
 	 */
-	public static boolean isWellFormed(File file) throws IOException {
+	private boolean isWellFormed(File file) throws IOException {
 		File schemaFile = new File("/src/test/resources/sample/MetadataValidatorSchema.xsd");
 		Source xmlFile = new StreamSource(file);
 		SchemaFactory schemaFactory = SchemaFactory
-		    .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		    .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);		
 		try {
 		  Schema schema = schemaFactory.newSchema(schemaFile);
 		  Validator validator = schema.newValidator();
@@ -86,117 +83,20 @@ public class MetadataValidation {
 		 */
 	}
 	
-	// Turns xml file into string for parsing or printing.
-	public static String fileToString(File file) throws FileNotFoundException, IOException{
-		BufferedReader reader = new BufferedReader(new FileReader(file));
-		String output = "", line = "";
-		while ((line = reader.readLine()) != null) {
-			output += line + "\n"; //correctness of adding new line characters here
-		}
-		output = output.substring(0, output.length() -2);
-		return output;
-	}
-	/*
-	 more comments and thoughts: temp file for validation
-	 have file local
-	 file with and without header lines - removed lines are as follows:
-	 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-		xsi:schemaLocation="http://www.w3schools.com/MetadataValidatorSchema metadata.xsd"
-		works without these two, not the first line:  xmlns="http://www.example.org/MetadataValidatorSchema"
-	now for making local file:
-	two lines removed targetNamespace="http://www.example.org/MetadataValidatorSchema" 
-xmlns:tns="http://www.example.org/MetadataValidatorSchema" 
-	these two lines removed from UD_German-PUD:
-			xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-		xsi:schemaLocation="http://www.w3schools.com/MetadataValidatorSchema metadata.xsd">
-	
-	//for changing, adding removing values: first parameter parse with old value, second parameter new value
-	
-	*/
-	/**
-	 * a lot of this method adapted from mkyong (https://www.mkyong.com/java/how-to-modify-xml-file-in-java-dom-parser/)  
-	 */
-	public static void changeAttributeForFile(File file, String parse, String oldValue, String newValue) throws FileNotFoundException{
-		// Idea of doing the changes in another class: reduce amount of imports and bulky methods here
-		try {
-			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-			Document doc = docBuilder.parse(file);
-
-			// Get the root element (always metadata)
-			Node rootNode = doc.getFirstChild();
-			
-			//follow parse input to get to relevant node
-			//potential ambiguity (more more complicated XML files): branching paths with the same-named nodes (and which one to select)
-			String[] parseList = parse.split(" "); // how are we splitting? check/confirm
-			Node current = rootNode;
-			for (String pathEntry : parseList) {
-				if (pathEntry == "metadata") // we have already moved here when we get the rootNode
-					continue;
-				else {
-					NodeList list = current.getChildNodes();
-					for (int i = 0; i < list.getLength(); i++) {
-						Node node = list.item(i);
-						if (pathEntry.equals(node.getNodeName())) {
-							System.out.println("This node along the path is: " + node.getNodeName());
-							// only ambiguous case (same-named nodes) is in showAttributes. Lazy solution: if we are at an "attribute" node,
-							// and the content is not oldValue, continue
-							if (node.getNodeName().equals("attribute") && !node.getTextContent().equals(oldValue))
-									continue;
-							current = node;
-							break;
-						}
-					}
-				}
-			}
-			current.setTextContent(newValue);
-			
-			// write the content into xml file
-			TransformerFactory transformerFactory = TransformerFactory.newInstance();
-			Transformer transformer = transformerFactory.newTransformer();
-			DOMSource source = new DOMSource(doc);
-			StreamResult result = new StreamResult(file);
-			transformer.transform(source, result);
-
-			System.out.println("Done");
-
-		   } catch (ParserConfigurationException pce) {
-			pce.printStackTrace();
-		   } catch (IOException ioe) {
-			ioe.printStackTrace();
-		   } catch (TransformerException tfe) {
-				tfe.printStackTrace();
-		   } catch (SAXException sae) {
-			sae.printStackTrace();
-		   }
-		}
-	/**
-	 * above (changeAttributeForFile) method, applied to a directory. Makes use of individual file.
-	 */
-	public static void changeAttributeForDirectory(File file, String parse, String oldValue, String newValue) throws FileNotFoundException {
-		if (file.isDirectory()) {
-			for (File fileEntry: file.listFiles()) {
-				if (fileEntry.getName().endsWith(".metadata"))
-					changeAttributeForFile(fileEntry, parse, oldValue, newValue);
-			}
-		}
-	}
-	
-	
 	public static void main(String args[]) throws IOException {
-		File folder = new File("src/test/resources/sample");
-		/**MetadataValidation mainValidator = new MetadataValidation(folder);
-		//mainValidator.isValidDirectory(folder);
+		File testFolder = new File("src/test/resources/sample");
+		File testFile = new File("src/test/resources/sample/UD_English-PUD_v2.3.metadata");
+		File testSchemaFile = new File("src/test/resources/sample/MetadataValidatorSchema.xsd");
+		//again, testing what java is reading as content of file. After checking (+ changing string -2 -> -1), we now know that they contain the expected!
+		MetadataModification modifier = new MetadataModification();
+		modifier.fileToString(testSchemaFile);
+		modifier.fileToString(testFile);
+		assert testSchemaFile.isFile();
+		assert testFolder.isFile();
+		assert testFile.isFile();
+
+		//MetadataValidation validator = new MetadataValidation(testFile); not working, fails at line 71. 
 		
-		System.out.println(alsoTestName.isFile());
-		System.out.println(testName.isFile());
-		*/ //nb 4 above print command working as intended
-		//File testName = new File("src/test/resources/sample/UD_English-PUD_v2.3.metadata");
-		File testName = new File("src/test/resources/sample");
-		File alsoTestName = new File("src/test/resources/MetadataValidatorSchema.xsd");
-		//System.out.println(isWellFormed(testName)); //THIS ERROR PROBLEM. in the meantime working on 
-		// System.out.println(fileToString(testName));
-		changeAttributeForDirectory(testName, "metadata showAttributes attribute", "pos", "newValue");		
+		modifier.changeAttributeForDirectory(testFile, "metadata showAttributes attribute", "pos", "newValue");		
 	}
-	
 }
